fb5bd0daacb2c6e52843a44aac6c1d8b0a4abc4c
Daniel Barbosa <danielmnbarbosa64@gmail.com>
2020-09-20 14:11:51 -0300

    Configuração inicial do projeto com Spring Initializr
    Usando H2 como database

---
+++ src/main/java/com/ufrn/imd/planob/planobapi/PlanobApiApplication.java
@@ -0,0 +1,13 @@
+package com.ufrn.imd.planob.planobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanobApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufrn/imd/planob/planobapi/PlanobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.imd.planob.planobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanobApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
